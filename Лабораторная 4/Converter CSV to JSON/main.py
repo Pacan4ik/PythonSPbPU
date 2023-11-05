d21a700eea5119dccd67ec897a36a162d3de9d9b
Andrey P <36331922+Pacan4ik@users.noreply.github.com>
2023-11-05 11:54:19 +0300

    lab4

---
+++ Лабораторная 4/Converter CSV to JSON/main.py
@@ -0,0 +1,28 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    data = []
+    ...  # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME, "r", newline="") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+
+    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
